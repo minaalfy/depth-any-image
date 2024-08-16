b1903fd95ae35b159c3f1fbc23650457de8dbd9f
Mina Paulis <5628914+minaalfy@users.noreply.github.com>
2024-08-16 16:46:36 -0700

    adjust deploy to githup pages

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/depth-any-image/sw.js', { scope: '/depth-any-image/' })})}
